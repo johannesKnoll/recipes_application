b84197faa9b8dc1103e5bde3cd8e9501b4c5a909
I516994 <johannes.knoll@sap.com>
2021-09-07 17:21:37 +0200

    Setup frontend

---
+++ Frontend/recipeApp/App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ScreenNavigation } from './screens/ScreenNavigation';
+
+export default function App() {
+  return (
+      <ScreenNavigation></ScreenNavigation>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
