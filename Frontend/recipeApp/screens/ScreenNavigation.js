b99bc2c2abd8f4159d291a345d9fa75a023f2a87
Abdul Jalil Lattouf <abdaljaleel.g@gmail.com>
2021-09-08 10:10:27 +0200

    tabNavigator

--- Frontend/recipeApp/screens/ScreenNavigation.js
+++ Frontend/recipeApp/screens/ScreenNavigation.js
@@ -1,32 +1,53 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Overview } from './overview';
+import { User } from './user';
+import { Favoriten } from './favoriten';
+import { Entdecken } from './entdecken';
+import { AddRecipe } from './addRecipe';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
 export function ScreenNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Übersicht') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          } else if (route.name === 'Favoriten') {
+            iconName = focused ? 'star' : 'star-outline';
+          }
+          else if (route.name === 'User') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+          else if (route.name === 'Hinzufügen') {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          }
+          else if (route.name === 'Entdecken') {
+            iconName = focused ? 'md-glasses' : 'md-glasses-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+        <Tab.Screen name="Übersicht" component={Overview} />
+        <Tab.Screen name="Favoriten" component={Favoriten} />
+        <Tab.Screen name="Entdecken" component={Entdecken} />
+        <Tab.Screen name="Hinzufügen" component={AddRecipe} />
+        <Tab.Screen name="User" component={User} />
+
       </Tab.Navigator>
     </NavigationContainer>
   );
