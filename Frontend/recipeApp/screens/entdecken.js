e23d5429e99e98806ec01724994a69eb1a07f254
I516994 <johannes.knoll@sap.com>
2021-09-27 19:00:10 +0200

    Favourites and discover page structure

--- Frontend/recipeApp/screens/entdecken.js
+++ Frontend/recipeApp/screens/entdecken.js
@@ -1,11 +1,130 @@
 
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { 
+    Text, 
+    View,
+    Image,
+    SafeAreaView,
+    TouchableOpacity,
+    TextInput,
+    FlatList
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import  RecipeCard  from '../components/RecipeCard';
+import RecentCard from '../components/RecentCard';
+import { SearchBar } from 'react-native-elements';
 
 export function Entdecken() {
+    const testData = [
+        {
+            id: 1,
+            name: "Test"
+        },
+        {
+            id: 2,
+            name: "Test2"
+        },
+        {
+            id: 3,
+            name: "Test3"
+        }
+    ];
+
+    // state = {
+    //     search: '',
+    //   };
+    const [search, setSearch] = React.useState("");
+
+    const updateSearch = (search) => {
+      setSearch(search);
+    };
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Entdecken!</Text>
-        </View>
+        <SafeAreaView
+            showHorizontalScrollIndicator={false}
+            showVerticalScrollIndicator={false}
+            style={{flex: 1, backgroundColor: "white"}}
+        >
+            <View
+                style={{
+                        margin: 15
+                    }}
+            >
+                <SearchBar
+                    lightTheme={true}
+                    placeholder="Type Here..."
+                    backgroundColor="white"
+                    onChangeText={updateSearch}
+                    value={search}
+                />
+            </View>
+            <View
+            style={{flex: 1, width: '100%'}}>
+                
+                    <View>
+                        <Text
+                            style={{
+                                marginLeft: 20,
+                                fontSize: 30,
+                                fontWeight: "bold",
+                                marginBottom: 10
+                            }}
+                        >
+                            Vegan
+                        </Text>
+                        <FlatList
+                            contentContainerStyle={{flexGrow: 1, paddingBottom: 5}}
+                            horizontal
+                            data={testData}
+                            keyExtractor={item => `${item.id}`}
+                            keyboardDismissMode="on-drag"
+                            showsHorizontalScrollIndicator={false}
+                            renderItem={({ item }) => {
+                                return (
+                                    <RecentCard
+                                        recipe={item}
+                                        onPress={null}
+                                    >
+                                    </RecentCard>
+                                )
+                            }}
+                            onEndReachedThreshold={0.5}>
+
+                        </FlatList>
+                    </View>
+                    <View>
+                        <Text
+                            style={{
+                                marginLeft: 20,
+                                fontSize: 30,
+                                fontWeight: "bold",
+                                marginBottom: 10
+                            }}
+                        >
+                            Vegatarisch
+                        </Text>
+                        <FlatList
+                            contentContainerStyle={{flexGrow: 1, paddingBottom: 5}}
+                            horizontal
+                            data={testData}
+                            keyExtractor={item => `${item.id}`}
+                            keyboardDismissMode="on-drag"
+                            showsHorizontalScrollIndicator={false}
+                            renderItem={({ item }) => {
+                                return (
+                                    <RecentCard
+                                        recipe={item}
+                                        onPress={null}
+                                    >
+                                    </RecentCard>
+                                )
+                            }}
+                            onEndReachedThreshold={0.5}>
+
+                        </FlatList>
+                    </View>
+            </View>
+        </SafeAreaView>
     );
 }
--- Frontend/recipeApp/screens/favoriten.js
+++ Frontend/recipeApp/screens/favoriten.js
@@ -1,11 +1,121 @@
 
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { 
+    Text, 
+    View,
+    Image,
+    SafeAreaView,
+    TouchableOpacity,
+    TextInput,
+    FlatList
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import  RecipeCard  from '../components/RecipeCard';
+import RecentCard from '../components/RecentCard';
+import { SearchBar } from 'react-native-elements';
 
 export function Favoriten() {
+    const testData = [
+        {
+            id: 1,
+            name: "Test"
+        },
+        {
+            id: 2,
+            name: "Test2"
+        },
+        {
+            id: 3,
+            name: "Test3"
+        }
+    ];
+
+    const [search, setSearch] = React.useState("");
+
+    const updateSearch = (search) => {
+      setSearch(search);
+    };
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Favoriten!</Text>
-        </View>
+        <SafeAreaView
+            showVerticalScrollIndicator={false}
+            showHorizontalScrollIndicator={false}
+            style={{flex: 1, backgroundColor: "white"}}
+        >
+            <View
+                style={{
+                        margin: 15
+                    }}
+            >
+                <SearchBar
+                    lightTheme={true}
+                    placeholder="Type Here..."
+                    backgroundColor="white"
+                    onChangeText={updateSearch}
+                    value={search}
+                />
+            </View>
+
+            <FlatList
+                data={testData}
+                keyExtractor={item => `${item.id}`}
+                keyboardDismissMode="on-drag"
+                showsHorizontalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View>
+                        <Text
+                            style={{
+                                marginLeft: 20,
+                                fontSize: 30,
+                                fontWeight: "bold",
+                                marginBottom: 10
+                            }}
+                        >
+                            Favoriten
+                        </Text>
+                        <FlatList
+                            data={testData}
+                            horizontal 
+                            showsHorizontalScrollIndicator={false}
+                            showVerticalScrollIndicator={false}
+                            keyExtractor={item => `${item.id}`}
+                            renderItem={({ item }) => {
+                                return (
+                                    <RecentCard
+                                        recipe={item}
+                                        onPress={null}
+                                    >
+                                    </RecentCard>
+                                )
+                            }}
+                        >
+                        </FlatList>
+                        <Text
+                            style={{
+                                marginLeft: 20,
+                                fontSize: 30,
+                                fontWeight: "bold",
+                                marginBottom: 10,
+                                marginTop: 35
+                            }}
+                        >
+                            Eigene Rezepte
+                        </Text>
+                    </View>
+                }
+                renderItem={({ item }) => {
+                    return (
+                        <RecipeCard
+                            recipe={item}
+                            onPress={null}
+                        >
+                        </RecipeCard>
+                    )
+                }}>
+
+            </FlatList>
+
+        </SafeAreaView>
     );
 }
