f609fa3b26e53c39e1130b745b97b3cb2eb406a3
Suliman Abu Ghaida <s.abughaida@outlook.de>
2021-09-30 02:33:03 +0200

    Rezept hinzufügen .css

--- Frontend/recipeApp/screens/addRecipe.js
+++ Frontend/recipeApp/screens/addRecipe.js
@@ -1,32 +1,10 @@
-
-// import * as React from 'react';
-// import { Text, View } from 'react-native';
-
-// export function AddRecipe() {
-//     return (
-//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//             <Text>AddRecipe!</Text>
-//         </View>
-//     );
-// }
-
-
-// import * as React from 'react';
-// import { Text, View } from 'react-native';
-
-// export function AddRecipe() {
-//     return (
-//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//             <Text>AddRecipe!</Text>
-//         </View>
-//     );
-// }
 import React, { Component } from 'react';
 import './AddRecipe.css';
-import {Link} from 'react-router-dom';
-import {Container, Button, Form, Input, Label, FormGroup} from 'reactstrap';
-import  ScreenNavigation  from './ScreenNavigation';
-import HomeScreen from '../components/HomeScreen';
+import { Link } from 'react-router-dom';
+import { Container, Button, Form, Input, Label, FormGroup } from 'reactstrap';
+import ScreenNavigation from './ScreenNavigation';
+import HomeScreen  from '../components/HomeScreen';
+
 
 class AddRecipe extends Component {
   emptyRecipe = {
@@ -35,7 +13,7 @@ class AddRecipe extends Component {
     image: '/recipes/recipe-background-alt.jpg',
     ingredients: 'Rice',
     preparation: 'Boil the rice',
-    category: {id: '1', name: 'Vegan'}
+    category: { id: '1', name: 'Vegan' }
   }
 
   constructor(props) {
@@ -57,39 +35,39 @@ class AddRecipe extends Component {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    let recipe = {...this.state.recipe};
+    let recipe = { ...this.state.recipe };
     recipe[name] = value;
-    this.setState({recipe});
+    this.setState({ recipe });
   }
 
   handleChangeImage(event) {
     const target = event.target;
     const value = target.files[0].name;
     const name = target.name;
-    let recipe = {...this.state.recipe};
+    let recipe = { ...this.state.recipe };
     recipe[name] = value;
-    this.setState({recipe});
+    this.setState({ recipe });
   }
 
   handleChangeCategory(event) {
     const target = event.target;
     const selectedIndex = target.selectedIndex + 1;
-    const value = target.value; 
+    const value = target.value;
     const name = target.name;
-    let recipe = {...this.state.recipe};
+    let recipe = { ...this.state.recipe };
     recipe[name].id = selectedIndex;
     recipe[name].name = value;
-    this.setState({recipe});
+    this.setState({ recipe });
   }
 
   async handleSubmit(event) {
     event.preventDefault();
-    const {recipe} = this.state;
+    const { recipe } = this.state;
     await fetch('http://localhost:8080/product/createProduct', {
       method: 'POST',
       headers: {
-        'Accept' : 'application/json',
-        'Content-Type' : 'application/json'
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(recipe)
     });
@@ -113,8 +91,8 @@ class AddRecipe extends Component {
   }
 
   render() {
-    const title = <h3 className="pt-2" style={{display: 'flex', justifyContent: 'center'}}>Add New Recipe</h3>
-    const {categories, isLoading} = this.state;
+    const title = <h3 className="pt-2" style={{ display: 'flex', justifyContent: 'center' }}>Add New Recipe</h3>
+    const { categories, isLoading } = this.state;
 
     // if(isLoading) {
     //   return (
@@ -126,15 +104,15 @@ class AddRecipe extends Component {
 
     return (
       <div className="Site">
-         <HomeScreen/>
+
         <div className="Home-image"></div>
         <Container className="Site-content">
           {title}
-         
-          <Form onSubmit={this.handleSubmit}>
+
+          <Form onSubmit={this.handleSubmit} className="test">
             <FormGroup>
               <Label for="name">Title</Label>
-              <Input type="name" name="name" id="name" onChange={this.handleChange} autoComplete="name"/>
+              <Input type="name" name="name" id="name" onChange={this.handleChange} autoComplete="name" />
             </FormGroup>
 
             <FormGroup>
@@ -144,29 +122,45 @@ class AddRecipe extends Component {
 
             <FormGroup>
               <Label for="category">Category</Label>
-              <select className="ml-1" name="category" id="category" onChange={this.handleChangeCategory}>
-      
-              </select>
+
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked />
+                <label class="form-check-label" for="exampleRadios1">
+                  Fleisch
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" />
+                <label class="form-check-label" for="exampleRadios2">
+                  Vegetariern
+                </label>
+              </div>
+              <div class="form-check disabled">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" />
+                <label class="form-check-label" for="exampleRadios3">
+                  Vegan
+                </label>
+              </div>
+            </FormGroup>
+            <FormGroup>
+              <HomeScreen/>
+              {/* <Label for="ingredients">Ingredients</Label>
+              <Input type="text" name="ingredients" id="ingredients" onChange={this.handleChange}/> */}
             </FormGroup>
 
             <FormGroup>
-              <Label for="ingredients">Ingredients</Label>
-              <Input type="text" name="ingredients" id="ingredients" onChange={this.handleChange}/>
-            </FormGroup>
-
-            <FormGroup>
-            <Label for="preparation">Preparation</Label>
-              <Input type="text" name="preparation" id="preparation" onChange={this.handleChange}/>
+              <Label for="preparation">Preparation</Label>
+              <Input type="text" name="preparation" id="preparation" onChange={this.handleChange} />
             </FormGroup>
 
             <FormGroup>
               <Button size="sm" color="primary" type="submit">Save Recipe</Button>
               <Button size="sm" className="ml-1" color="secondary" >Cancel</Button>
             </FormGroup>
-            
+
           </Form>
         </Container>
-     
+
       </div>
     );
   }
