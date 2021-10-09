ef5d09b606843d721f29efd3546541be7b0f5ada
Abdul Jalil Lattouf <abdaljaleel.g@gmail.com>
2021-10-09 11:44:07 +0200

    Touchable Highlight

--- Frontend/recipeApp/components/FooterMenu.js
+++ Frontend/recipeApp/components/FooterMenu.js
@@ -3,6 +3,7 @@ import {
     Text,
     View,
     TouchableOpacity,
+    TouchableHighlight,
     Image
 } from 'react-native';
 import { Icon } from 'react-native-elements';
@@ -21,59 +22,93 @@ const FooterMenu = ({ onPressHome, onPressFavoriten, onPressEntdecken, onPressHi
                 justifyContent: 'center',
             }}
         >
-            <Ionicons
-                style={{
-                    marginLeft: 40,
-                    marginRight: 40,
-                    top: '50%'
-                }}
-                name="home"
-                size={40}
-                color="white"
-                onPress={onPressHome}>
-            </Ionicons>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor='tomato'
+                onPress={() => alert('Pressed!')}>
+                <Ionicons
+                    style={{
+                        marginLeft: 30,
+                        marginRight: 30,
+                        top: '50%'
+                    }}
+                    name="home"
+                    size={40}
+                    color="white"
+                    onPress={onPressHome}>
+                </Ionicons>
+            </TouchableHighlight>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor='tomato'
+                onPress={() => alert('Pressed!')}>
 
-            <Ionicons
-                style={{
-                    marginRight: 40,
-                    top: '50%'
-                }}
-                name="star"
-                size={40}
-                color="white"
-                onPress={onPressFavoriten}>
-            </Ionicons>
-            <Ionicons
-                style={{
-                    marginRight: 40,
-                    top: '50%'
-                }}
+                <Ionicons
+                    style={{
+                        marginRight: 30,
+                        marginLeft: 30,
+                        top: '50%'
+                    }}
+                    name="star"
+                    size={40}
+                    color="white"
+                    onPress={onPressFavoriten}>
+                </Ionicons>
+            </TouchableHighlight>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor='tomato'
+                onPress={() => alert('Pressed!')}>
+                <Ionicons
+                    style={{
+                        marginRight: 30,
+                        marginLeft: 30,
+                        top: '50%'
+                    }}
 
-                name="map-outline"
-                size={40}
-                color="white"
-                onPress={onPressEntdecken}>
-            </Ionicons>
-            <Ionicons
-                style={{
-                    marginRight: 40,
-                    top: '50%'
-                }}
-                name="add-circle"
-                size={40}
-                color="white"
-                onPress={onPressHinzufuegen}>
-            </Ionicons>
-            <Ionicons
-                style={{
-                    marginRight: 40,
-                    top: '50%'
-                }}
-                name='person'
-                size={40}
-                color="white"
-                onPress={onPressUser}>
-            </Ionicons>
+                    name="map-outline"
+                    size={40}
+                    color="white"
+                    onPress={onPressEntdecken}>
+                </Ionicons>
+            </TouchableHighlight>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor='tomato'
+                onPress={() => alert('Pressed!')}>
+                <Ionicons
+                    style={{
+                        marginRight: 30,
+                        marginLeft: 30,
+                        top: '50%'
+                    }}
+                    name="add-circle"
+                    size={40}
+                    color="white"
+                    onPress={onPressHinzufuegen}>
+                </Ionicons>
+            </TouchableHighlight>
+            <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor='tomato'
+                onPress={() => alert('Pressed!')}>
+                <Ionicons
+                    style={{
+                        marginRight: 30,
+                        marginLeft: 30,
+                        top: '50%'
+                    }}
+                    name='person'
+                    size={40}
+                    color="white"
+                    onPress={onPressUser}>
+                </Ionicons>
+            </TouchableHighlight>
+
+
+
+
+
         </View>
     )
 }
